7ce94e80dae3a35caa0b3b666f7041a3329a3c4b
pingping260687005 <pingping_01@hotmail.com>
2018-08-31 00:30:14 +0800

    UI for bootstrap table and toolbar funcitons

--- src/app/student-management/student-management.component.ts
+++ src/app/student-management/student-management.component.ts
@@ -11,6 +11,9 @@ import { Observable, from } from 'rxjs/index';
 })
 export class StudentManagementComponent implements OnInit {
   dataSource: Observable<any>;
+  private addModifyDialogTitle = '';
+  private isModifyBtnDisabled = true;
+  private isDeleteBtnDisabled = true;
   constructor(private http: Http) {
     //用http请求
     this.dataSource = this.http.get('/api/products');
@@ -33,6 +36,10 @@ export class StudentManagementComponent implements OnInit {
   ngAfterViewInit() {
     $('#studentMngTable').bootstrapTable({
       columns: [{
+        checkbox: true,
+        visiable: true
+      },
+      {
         field: 'id',
         title: 'Item ID',
         visible: false
@@ -61,41 +68,89 @@ export class StudentManagementComponent implements OnInit {
         field: 'videoId',
         title: '视频页'
       }],
-      data: this.getStudentList()
+      data: this.getStudentList(),
+      search: true,
+      pagination: true,
+      pageSize: 15,
+      idField: "id",
+      uniqueId: "id",
+      smartDisplay: true,
+      checkboxHeader: true,
+      clickToSelect: true,
+      toolbar: "#toolbar",
+      onCheck: () => {
+        this.updateToolbarIconsStatus();
+      },
+      onUncheck: () => {
+        this.updateToolbarIconsStatus();
+      }
     });
-
-
-
+    this.updateToolbarIconsStatus();
   }
 
   private getStudentList(): Student[] {
     ////////hard code////////////////
     let studentList: Student[] = [];
-    let student1: Student = {
-      id: "1",
-      studentNum: "00011",
-      name: "李红",
-      sex: "女",
-      age: 10,
-      phone: '13992288771',
-      parentPhone: '13992288771',
-      address: '丹阳市黄金路25弄16号201室'
-    };
-    let student2: Student = {
-      id: "2",
-      studentNum: "00012",
-      name: "王刚",
-      sex: "男",
-      age: 9,
-      phone: '13992288771',
-      parentPhone: '13992288771',
-      address: '丹阳市黄金路25弄16号201室'
-    };
-    studentList.push(student1);
-    studentList.push(student2);
+    let student: Student;
+    for (let i = 0; i < 100; i++) {
+      student = {
+        id: i.toString(),
+        studentNum: i.toString(),
+        name: "随机 " + i,
+        sex: i % 2 === 0 ? "男" : "女",
+        age: Math.floor(Math.random() * 100),
+        phone: '13992288771',
+        parentPhone: '13992288771',
+        address: '丹阳市黄金路25弄16号201室'
+      };
+      studentList.push(student);
+    }
     return studentList;
   }
 
+  private showModal(isAdd: boolean) {
+    let modal = $("#addOrModifyModal");
+    if (isAdd) {
+      this.addModifyDialogTitle = "添加学生信息";
+    } else {
+      this.addModifyDialogTitle = "修改学生信息";
+      let selection = $("#studentMngTable").bootstrapTable("getSelections", null)[0]; // 修改只能是一条数据，所以直接用第一个
+      modal.find(".studentNum").val(selection["studentNum"]);
+      modal.find(".phone").val(selection["phone"]);
+      modal.find(".name").val(selection["name"]);
+      modal.find(".parentPhone").val(selection["parentPhone"]);
+      modal.find(".age").val(selection["age"]);
+      modal.find(".sex").val(selection["sex"]);
+      modal.find(".address").val(selection["address"]);
+      modal.find(".address").attr("title", selection["address"]);
+    }
+    modal.modal("show");
+  }
+
+  private updateToolbarIconsStatus() {
+    let selectionsLength = $("#studentMngTable").bootstrapTable("getSelections", null).length;
+    this.isModifyBtnDisabled = selectionsLength !== 1;
+    this.isDeleteBtnDisabled = selectionsLength === 0;
+    if (this.isModifyBtnDisabled) {
+      $("#modifyBtn").addClass("disabled");
+    } else {
+      $("#modifyBtn").removeClass("disabled");
+    }
+    if (this.isDeleteBtnDisabled) {
+      $("#deleteBtn").addClass("disabled");
+    } else {
+      $("#deleteBtn").removeClass("disabled");
+    }
+  }
+
+  private removeItems() {
+    let table = $("#studentMngTable");
+    let selections = table.bootstrapTable("getSelections", null);
+    for (let i = 0; i < selections.length; i++) {
+      table.bootstrapTable("removeByUniqueId", selections[i].id);
+    }
+    $("#deleteBtn").addClass("disabled");
+  }
 
   ngOnDestroy() {
     $("#studentMngTable").bootstrapTable('destroy');
