e99198594ca26439401bcd4bba304f3cf48de958
胡仁杰 <hurj8@chinaunicom.com>
2018-11-10 21:47:03 +0800

    style update

--- src/app/vedio-detail-management/vedio-detail-management.component.ts
+++ src/app/vedio-detail-management/vedio-detail-management.component.ts
@@ -10,6 +10,14 @@ export class VedioDetailManagementComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('#input-b9').fileinput({
+      language: 'zh', // 设置语言
+      showPreview: true,
+      showUpload: false,
+      elErrorContainer: '#kartik-file-errors',
+      allowedFileExtensions: ['jpg', 'png', 'gif']
+      // uploadUrl: '/site/file-upload-single'
+  });
   }
 
 }
